fbc945c7342b3a593d5282ae7266d84aa51cef22
Dominik Zyla <dominik.zyla@gmail.com>
2015-09-24 01:22:06 +0100

    Change datastructures

--- xml/datastructs.go
+++ xml/datastructs.go
@@ -1,17 +1,30 @@
 // Package main provides ...
 package xml
 
+//TODO, some defaults should be used here.
 type OpenChecksumType struct {
 	Type	string	`xml:"type,attr,omitempty"`
 	Value	string	`xml:",chardata"`
 }
 
+type ChecksumType struct {
+	Type	string	`xml:"type,attr,omitempty"`
+	Value	string	`xml:",chardata"`
+}
+
+type LocationType struct {
+	Href	string	`xml:"href,attr,omitempty"`
+	Value	string	`xml:",chardata"`
+}
+
 type DataType struct {
-	Type			string		`xml:"type,attr,omitempty"`
-	Location		string		`xml:"location"`
-	Checksum		string		`xml:"checksum"`
-	Timestamp		string		`xml:"timestamp"`
-	OpenChecksum	OpenChecksumType	`xml:"open-checksum"`
+	Type			string					`xml:"type,attr,omitempty"`
+	Location		OpenChecksumType		`xml:"location"`
+	Checksum		ChecksumType			`xml:"checksum"`
+	Timestamp		string					`xml:"timestamp"`
+	OpenChecksum	OpenChecksumType		`xml:"open-checksum"`
+	Size			int						`xml:"size"`
+	OpenSize		int						`xml:"open-size"`
 }
 
 type Header struct {
